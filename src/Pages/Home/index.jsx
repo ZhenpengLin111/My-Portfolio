c301abeba5f95d89e1a6dc4dd59908a6195cb4cc
ZPL123 <penglin524@gmail.com>
2024-07-04 21:35:26 -0400

    add animations for home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -16,6 +16,7 @@ import Icon from "./icon";
 import Footer from "@/Components/Footer";
 import ScrollBtn from "@/Components/ScrollBtn";
 
+
 function Home() {
     const chartRef = useRef(null)
     const chartContainer = useRef(null)
@@ -143,6 +144,37 @@ function Home() {
     const handleScroll = (e) => {
         const { scrollTop } = e.target
         setScrollPos(scrollTop)
+        handleSectionVis()
+    }
+
+    // Visibilities of different sections
+    const [aboutMeVis, setAboutMeVis] = useState(false)
+    const [projectsVis, setProjectsVis] = useState(false)
+    const [skillsVis, setSkillsVis] = useState(false)
+    const [contactVis, setContactVis] = useState(false)
+
+    const handleSectionVis = () => {
+        const aboutMe = document.querySelector('.About-Me')
+        const myProjects = document.querySelector('.My-Projects')
+        const mySkills = document.querySelector('.My-Skills')
+        const contact = document.querySelector('.Contact')
+        if (aboutMe) {
+            const sectionTop = aboutMe.getBoundingClientRect().top
+            setAboutMeVis(sectionTop <= window.innerHeight / 2)
+        }
+        if (myProjects) {
+            const sectionTop = myProjects.getBoundingClientRect().top
+            setProjectsVis(sectionTop <= window.innerHeight / 2)
+        }
+        if (mySkills) {
+            const sectionTop = mySkills.getBoundingClientRect().top
+            setSkillsVis(sectionTop <= window.innerHeight / 2)
+        }
+        if (contact) {
+            const sectionTop = contact.getBoundingClientRect().top
+            setContactVis(sectionTop <= window.innerHeight / 2)
+        }
+
     }
 
     return (
@@ -168,15 +200,15 @@ function Home() {
                     <img src={Homeimg} alt=""></img>
                 </div>
             </div>
-            <div className="About-Me">
-                <h1>About Me</h1>
-                <p>I am passionate about full-stack development and dedicated to creating engaging user experiences. While I am still in the early stages of my journey, I have been actively learning and practicing key full-stack technologies. I am particularly interested in responsive design and improving web performance.</p>
-                <p>My background is in Computer Science, where I gained a solid understanding of programming principles, algorithms, and data structures. During my studies, I developed a keen interest in web development, particularly in creating interactive and aesthetically pleasing user interfaces. I have been channeling this interest into learning full-stack development through online courses, tutorials, and personal projects.</p>
-                <p>My goal is to become a proficient full-stack developer, capable of building seamless and engaging web experiences. I am committed to continuous learning and improvement, and I am excited to contribute to innovative projects in the web development field.</p>
+            <div className={`About-Me ${aboutMeVis ? 'fade-in' : ''}`}>
+                <h1 className={`${aboutMeVis ? 'fade-in-bottom' : ''}`}>About Me</h1>
+                <p className={`${aboutMeVis ? 'fade-in-bottom' : ''}`}>I am passionate about full-stack development and dedicated to creating engaging user experiences. While I am still in the early stages of my journey, I have been actively learning and practicing key full-stack technologies. I am particularly interested in responsive design and improving web performance.</p>
+                <p className={`${aboutMeVis ? 'fade-in-bottom' : ''}`}>My background is in Computer Science, where I gained a solid understanding of programming principles, algorithms, and data structures. During my studies, I developed a keen interest in web development, particularly in creating interactive and aesthetically pleasing user interfaces. I have been channeling this interest into learning full-stack development through online courses, tutorials, and personal projects.</p>
+                <p className={`${aboutMeVis ? 'fade-in-bottom' : ''}`}>My goal is to become a proficient full-stack developer, capable of building seamless and engaging web experiences. I am committed to continuous learning and improvement, and I am excited to contribute to innovative projects in the web development field.</p>
             </div>
-            <div className="My-Projects">
-                <h1>My Projects</h1>
-                <div className="Projects-container">
+            <div className={`My-Projects ${projectsVis ? 'fade-in' : ''}`}>
+                <h1 className={`${projectsVis ? 'fade-in-x' : ''}`}>My Projects</h1>
+                <div className={`Projects-container ${projectsVis ? 'fade-in-x' : ''}`}>
                     <ul>
                         <Work
                             link={"https://zplarticlemanager.netlify.app"}
@@ -209,17 +241,17 @@ function Home() {
                     </ul>
                 </div>
             </div>
-            <div className="My-Skills">
-                <h1>My Skills</h1>
-                <div ref={chartContainer} style={{ width: '100%', height: '75vh' }}></div>
+            <div className={`My-Skills ${skillsVis ? 'fade-in' : ''}`}>
+                <h1 className={skillsVis ? 'fade-in-x' : ''}>My Skills</h1>
+                <div className={`skillChart ${skillsVis ? 'fade-in-x' : ''}`} ref={chartContainer} style={{ width: '100%', height: '75vh' }}></div>
             </div>
-            <div className="Contact">
-                <h1>Contact</h1>
-                <p>I am actively seeking internship and job opportunities in full-stack and web development. If you have any openings or would like to discuss potential collaborations, please feel free to reach out to me. I look forward to connecting with you!</p>
-                <a href="mailto:penglin524@gmail.com" className="Hello-btn">
+            <div className={`Contact ${contactVis ? 'fade-in' : ''}`}>
+                <h1 className={contactVis ? 'fade-in-bottom' : ''}>Contact</h1>
+                <p className={contactVis ? 'fade-in-bottom' : ''}>I am actively seeking internship and job opportunities in full-stack and web development. If you have any openings or would like to discuss potential collaborations, please feel free to reach out to me. I look forward to connecting with you!</p>
+                <a href="mailto:penglin524@gmail.com" className={`Hello-btn ${contactVis ? 'fade-in-bottom' : ''}`}>
                     Say Hello
                 </a>
-                <ul className="Contact-Icons">
+                <ul className={`Contact-Icons ${contactVis ? 'fade-in-bottom' : ''}`}>
                     <Icon href={"https://github.com/ZhenpengLin111"} icon={faGithub} />
                     <Icon href={"https://www.linkedin.com/in/zhenpeng-lin-317621227"} icon={faLinkedin} />
                     <Icon href={"https://space.bilibili.com/387132771?spm_id_from=333.1007.0.0"} icon={faBilibili} style={{ marginRight: '0px' }} />
